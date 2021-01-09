3d33b2da24c776a75686eaee112f0fe6fd69038f
arifbsl <ahak.bsl@gmail.com>
2021-01-09 19:01:14 +0600

    light mode and dark mood done

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
 const toggleButton = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+// Image color change
+function imageMood(mood) {
+  image1.src = `img/undraw_proud_coder_${mood}.svg`;
+  image2.src = `img/undraw_feeling_proud_${mood}.svg`;
+  image3.src = `img/undraw_conceptual_idea_${mood}.svg`;
+}
+
+
+// Dark mood
+function darkMood() {
+  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+
+  toggleIcon.children[0].textContent = "Dark Mood";
+  toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
+  imageMood('dark')
+}
+
+// Light mood
+function lightMood() {
+  nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+
+  toggleIcon.children[0].textContent = "Light Mood";
+  toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
+  imageMood('light')
+}
 
 
 // Switch Theme Dynamically
-
 function switchTheme(event) {
   if (event.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
+    darkMood();
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
+    lightMood();
   }
 }
 
