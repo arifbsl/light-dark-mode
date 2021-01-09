d3fff7aec5eb1f3af765a2d75b6ba957afbfd3ad
arifbsl <ahak.bsl@gmail.com>
2021-01-09 17:53:23 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const toggleButton = document.querySelector('input[type="checkbox"]');
+
+
+// Switch Theme Dynamically
+
+function switchTheme(event) {
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+}
+
+
+
+// Add event listeners  on toggleButton
+toggleButton.addEventListener('change', switchTheme);
